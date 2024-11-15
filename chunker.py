2016fccb7a04ac4ec71e9b40f6f2127262b1afdb
vihaannnn <vihaan.nama@outlook.com>
2024-11-15 16:23:48 -0500

    Token Wise Chunking Done

--- chunker.py
+++ chunker.py
@@ -8,19 +8,27 @@ def write_chunks_to_file(chunks, output_file, separator="\n---\n"):
                 file.write(separator)
 
 
+with open('./RawFiles/output1.txt', 'r', encoding='utf-8') as file:
+    # Read the entire contents of the file
+    text = file.read()
+
+
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=200
 )
 
-with open('./RawFiles/output1.txt', 'r', encoding='utf-8') as file:
-    # Read the entire contents of the file
-    text = file.read()
-
 chunks = text_splitter.split_text(text)
 
-for chunk in chunks:
-    print(chunk)
-    print("*************************************")
+# for chunk in chunks:
+#     print(chunk)
+#     print("*************************************")
 
 write_chunks_to_file(chunks=chunks, output_file="./Recursive/Recusrive-Chunker-1.txt")
+
+from langchain.text_splitter import TokenTextSplitter
+
+text_splitter = TokenTextSplitter(chunk_size=100, chunk_overlap=20)
+chunks = text_splitter.split_text(text)
+
+write_chunks_to_file(chunks=chunks, output_file="./TokenWise/Token-Chunker-1.txt")
